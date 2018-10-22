71165bf99922cd3959a3db95ec8611ee1695c278
said shah <saidmysr20@aol.com>
2018-10-22 09:04:34 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
-
+#adding a name to the table 
 def change
   create_table :students do |t|
     t.text :name
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-
+  #changing a column 
   def change
     change_column :students, :birthdate, :DateTime
   end
